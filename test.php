60fcb20524931c3220411b8817deb037b8729b79
jsguy <mikkel@pointful.com>
2013-09-25 16:26:59 +1000

    Added slightly less-worse example

--- test.php
+++ test.php
@@ -16,9 +16,28 @@ for($i = 0; $i < $num; $i += 1) {
 }
 ?>
 
-<div id="progress">0%</div>
+<p>
+	Below you'll see a progress that counts up to 100%, without crashing the browser.<br/>
+	The code is inserting 4 spans in each of 30,000 DIVs, and depending on your machine/browser, it will finish slower than jQuery.each could do it, but without crashing.<br/>
+	On my machine (i5 2.5Ghz "Mobile", 8GB RAM), in Chrome 29.0.1547.76 it finishes in ~ 7200ms.
+</p>
+
+<p>
+Progress: <span id="progress">0%</span><span id="results"></span>
+</p>
+
+<p>
+	Click the button below to run it again!<br/>
+	<button type="button" id="nocrash">Click here to run test</button>
+</p>
+
+<p>
+	Click the button below to either see the dreaded "Unresponsive script" error, or if your browser/machine is decent, wait while the page is seemingly frozen - you won't see any progress updates. It is running the same function, but using jQuery.each instead.<br/>
+	<button type="button" id="snowcrash">Click here to crash your browser</button>
+</p>
 
 <script>
+//	Poorly written JS test, enjoy.
 var progress = function(idx){
 		var total = <?php print $num; ?>,
 			pct = parseInt((idx/total)*100, 10);
@@ -30,13 +49,62 @@ var progress = function(idx){
 			$(this).find('span').html('<span>Updated DOM</span>');
 		}
 		progress(idx + 1);
+	},
+	//	Ensure we only run one instance
+	isRunning = false,
+	startTime,
+	cleanUp = function(){
+		$('#nocrash').removeProp('disabled');
+		$('#snowcrash').removeProp('disabled');
+		$('#results').html(' Inserted 120,000 spans in ' + ((new Date()).getTime() - startTime) + 'ms');
+		isRunning = false;
+	},
+	runSilly = function(useJquery){
+		if(isRunning){
+			window.console && console.log('Already running, plz wait.');
+			return;
+		}
+
+		startTime = (new Date()).getTime();
+
+		$('#nocrash').prop({'disabled': 'disabled'});
+		$('#snowcrash').prop({'disabled': 'disabled'});
+		$('#results').html('');
+		isRunning = true;
+
+		setTimeout(function(){
+			if(useJquery) {
+				$('div').each(sillyDom);
+				cleanUp();
+			} else {
+				$('div').nice(sillyDom, cleanUp);
+			}
+		}, 0);
 	};
 
-//	WARNING: might crash your browser!
-//$('div').each(sillyDom);
+//	Read from the hash
+var runType = window.document.location.hash;
 
-//	Works!
-$('div').nice(sillyDom);
+if(runType !== "") {
+	if(runType == "#snowcrash") {
+		runSilly(true);
+	} else {
+		runSilly();
+	}
+}
+
+
+$('#nocrash').click(function(){
+	window.document.location.hash = "nocrash";
+	window.document.location.reload();
+});
+
+$('#snowcrash').click(function(){
+	if(confirm("Are you sure you want to crash your browser? You won't be warned again...")) {
+		window.document.location.hash = "snowcrash";
+		window.document.location.reload();
+	}
+});
 
 </script>
 
